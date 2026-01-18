7166c5d8d912c3e3a4b6d2ec42387129805b40b0
Tomash25 <164175207+Tomash25@users.noreply.github.com>
2026-01-18 17:48:06 +0200

    Make typing_tools._is_union_type public

--- foxhound/core/typing_tools.py
+++ foxhound/core/typing_tools.py
@@ -7,18 +7,18 @@ def validate_concrete_parameters(signature: inspect.Signature) -> None:
     for name, parameter in signature.parameters.items():
         if parameter.annotation == inspect.Parameter.empty:
             raise TypeError(f'Parameter "{name}" is missing type hinting annotation')
-        if _is_union_type(parameter.annotation):
+        if is_union_type(parameter.annotation):
             raise TypeError(f'Parameter "{name}" is type hinted as a union type')
 
 
 def validate_concrete_return_type(signature: inspect.Signature) -> None:
     if signature.return_annotation == inspect.Signature.empty:
         raise TypeError('Signature is missing a return type hinting annotation')
-    if _is_union_type(signature.return_annotation):
+    if is_union_type(signature.return_annotation):
         raise TypeError('Return type is hinted as a union type')
 
 
-def _is_union_type(annotation: Any) -> bool:
+def is_union_type(annotation: Any) -> bool:
     origin: type = get_origin(annotation)
     return origin is Union or (hasattr(origin, '__name__') and origin.__name__ == 'UnionType')
 
