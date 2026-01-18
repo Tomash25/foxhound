a3f7e1dd1ced8a7273783c6fe4d1c8d7fb0e4008
Tomash25 <164175207+Tomash25@users.noreply.github.com>
2026-01-18 15:25:51 +0200

    Prohibit use of union types in DI function signatures

--- foxhound/core/signature_tools.py
+++ foxhound/core/signature_tools.py
@@ -1,14 +1,26 @@
 import inspect
 from inspect import Signature
-from typing import Any
+from typing import Any, Union, get_origin
 
 
-def parameters_hinted(signature: Signature) -> bool:
-    return all(parameter.annotation != inspect.Parameter.empty for parameter in signature.parameters.values())
+def validate_concrete_parameters(signature: inspect.Signature) -> None:
+    for name, parameter in signature.parameters.items():
+        if parameter.annotation == inspect.Parameter.empty:
+            raise TypeError(f'Parameter "{name}" is missing type hinting annotation')
+        if _is_union_type(parameter):
+            raise TypeError(f'Parameter "{name}" is type hinted as a union type')
 
 
-def fully_hinted(signature: Signature):
-    return parameters_hinted(signature) and signature.return_annotation != inspect.Signature.empty
+def validate_concrete_return_type(signature: inspect.Signature) -> None:
+    if signature.return_annotation == inspect.Signature.empty:
+        raise TypeError('Signature is missing a return type hinting annotation')
+    if _is_union_type(signature.return_annotation):
+        raise TypeError('Return type is hinted as a union type')
+
+
+def _is_union_type(annotation: Any) -> bool:
+    origin: type = get_origin(annotation)
+    return origin is Union or (hasattr(origin, '__name__') and origin.__name__ == 'UnionType')
 
 
 def simplify_parameters(signature: Signature) -> dict[str, type[Any]]:
