8ec53e7617165636d1904bcd67ad53d661adc788
Tomash25 <164175207+Tomash25@users.noreply.github.com>
2026-03-31 22:48:45 +0300

    Use Result in dependency_resolver.py

--- foxhound/core/dependency_resolver.py
+++ foxhound/core/dependency_resolver.py
@@ -1,62 +1,55 @@
 from abc import ABC, abstractmethod
+from types import GenericAlias
 
-from foxhound import ComponentDefinition
+from foxhound import ComponentDefinition, Result
 from foxhound.core.model.parameter import Parameter
 from foxhound.core.typing_tools import is_assignable_to
 
 
 class DependencyResolver(ABC):
     @abstractmethod
-    def try_resolve(
-            self,
-            dependency: Parameter,
-            candidates: list[ComponentDefinition]
-    ) -> tuple[str | None, str | None]:
+    def try_resolve(self, dependency: Parameter, candidates: list[ComponentDefinition]) -> Result[str]:
         ...
 
 
 class BasicDependencyResolver(DependencyResolver):
-    def try_resolve(
-            self,
-            dependency: Parameter,
-            candidates: list[ComponentDefinition]
-    ) -> tuple[str | None, str | None]:
+    def try_resolve(self, dependency: Parameter, candidates: list[ComponentDefinition]) -> Result[str]:
         if dependency.qualifier is None:
-            return self._find_unqualified_component(dependency.kind, candidates)
+            return self._find_unqualified_component(dependency, candidates)
 
-        return self._find_qualified_component(dependency.kind, dependency.qualifier, candidates)
+        return self._find_qualified_component(dependency, candidates)
 
-    def _find_qualified_component(
-            self,
-            kind: type,
-            qualifier: str,
-            candidates: list[ComponentDefinition]
-    ) -> tuple[str | None, str | None]:
+    def _find_qualified_component(self, dependency: Parameter, candidates: list[ComponentDefinition]) -> Result[str]:
+        kind: type | GenericAlias = dependency.kind
+        qualifier: str = dependency.qualifier
+
         type_matches: list[ComponentDefinition] = self._filter_matching_candidates(kind, candidates)
 
         if len(type_matches) == 0:
-            return None, (f'No registered component matching {kind} with qualifier "{qualifier}". '
-                          f'In fact, no component matching {kind} has been found at all.')
+            return Result.fail(
+                f'No registered component matching {kind} with qualifier "{qualifier}". '
+                f'In fact, no component matching {kind} has been found at all.'
+            )
 
         for candidate in type_matches:
             if candidate.component_metadata.qualifier == qualifier:
-                return candidate.id, None
+                return Result.ok(candidate.id)
 
-        return None, (f'No registered component matching {kind} with qualifier "{qualifier}". '
-                      f'However, {len(type_matches)} other components matching {kind} are registered.')
+        return Result.fail(
+            f'No registered component matching {kind} with qualifier "{qualifier}". '
+            f'However, {len(type_matches)} other components matching {kind} are registered.'
+        )
 
-    def _find_unqualified_component(
-            self,
-            kind: type,
-            candidates: list[ComponentDefinition]
-    ) -> tuple[str | None, str | None]:
+    def _find_unqualified_component(self, dependency: Parameter, candidates: list[ComponentDefinition]) -> Result[str]:
+        kind: type | GenericAlias = dependency.kind
+
         type_matches: list[ComponentDefinition] = self._filter_matching_candidates(kind, candidates)
 
         if len(type_matches) == 0:
-            return None, f'No registered component matching {kind}'
+            return Result.fail('No registered component matching {kind}')
 
         if len(type_matches) == 1:
-            return type_matches[0].id, None
+            return Result.ok(type_matches[0].id)
 
         primary_matches: list[ComponentDefinition] = [
             candidate for candidate in type_matches
@@ -64,16 +57,20 @@ class BasicDependencyResolver(DependencyResolver):
         ]
 
         if len(primary_matches) == 1:
-            return primary_matches[0].id, None
+            return Result.ok(primary_matches[0].id)
 
         if len(primary_matches) > 1:
-            return None, (f'Multiple components matching {kind} were found, '
-                          f'and multiple of them are marked as primary. '
-                          f'Specific component can be selected by specifying a qualifier, '
-                          f'or exactly one primary component.')
+            return Result.fail(
+                f'Multiple components matching {kind} were found, '
+                f'and multiple of them are marked as primary. '
+                f'Specific component can be selected by specifying a qualifier, '
+                f'or exactly one primary component.'
+            )
 
-        return None, (f'Multiple components matching {kind} were found. Specific component '
-                      f'can be selected by specifying a qualifier or a primary component.')
+        return Result.fail(
+            f'Multiple components matching {kind} were found. Specific component '
+            f'can be selected by specifying a qualifier or a primary component.'
+        )
 
     def _filter_matching_candidates(
             self,
