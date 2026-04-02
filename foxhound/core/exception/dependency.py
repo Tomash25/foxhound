f82d16d7090e9a9239a19e50c89367616d2f2dac
Tomash25 <164175207+Tomash25@users.noreply.github.com>
2026-04-03 01:02:11 +0300

    Create UnsatisfiedDependenciesError

---
+++ foxhound/core/exception/dependency.py
@@ -0,0 +1,8 @@
+from foxhound.core.model.parameter import Parameter
+
+
+class UnsatisfiedDependenciesError(Exception):
+    def __init__(self, hints: dict[Parameter, str]):
+        self.hints = hints
+        listed_hints: str = ''.join([f'\n\t{parameter.name}: {hint}' for parameter, hint in hints.items()])
+        super().__init__(f'Dependencies unsatisfied: ' + listed_hints)
