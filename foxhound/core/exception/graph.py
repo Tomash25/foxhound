52021d194cfbe78503e590fd9b5519e2fd9e86c9
Tomash25 <164175207+Tomash25@users.noreply.github.com>
2026-04-03 02:02:58 +0300

    Add first element last in CyclicGraphError formatted_cycles

--- foxhound/core/exception/graph.py
+++ foxhound/core/exception/graph.py
@@ -9,7 +9,7 @@ class CyclicGraphError(Exception):
     cycles: list[Hashable | Any]
 
     def __init__(self, graph: DiGraph):
-        formatted_cycles: list[str] = [' → '.join(c) for c in simple_cycles(graph)]
         self.graph = graph
         self.cycles = list(simple_cycles(graph))
+        formatted_cycles: list[str] = [' → '.join(c + c[:1]) for c in self.cycles]
         super().__init__(f'Cyclic dependencies detected: {"; ".join(formatted_cycles)}')
