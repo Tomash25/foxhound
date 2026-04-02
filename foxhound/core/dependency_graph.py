96ebc2842c6a6c6de00ec4a0f8f1fa84e5a2bfea
Tomash25 <164175207+Tomash25@users.noreply.github.com>
2026-04-02 19:59:12 +0300

    Use DependencyResolver in DependencyGraphMapper

--- foxhound/core/dependency_graph.py
+++ foxhound/core/dependency_graph.py
@@ -1,14 +1,12 @@
-import inspect
 from enum import Enum
-from typing import Any
 
 from networkx import DiGraph
 
-from foxhound import ComponentMetadata
+from foxhound.core.model.result import Result
+from foxhound.core.dependency_resolver import DependencyResolver
 from foxhound.core.model.component_definition import ComponentDefinition
 from foxhound.core.model.parameter import Parameter
 from foxhound.core.parameter_tools import parse_parameters
-from foxhound.core.typing_tools import simplify_arguments, is_assignable_to
 
 
 class NodeType(Enum):
@@ -17,12 +15,15 @@ class NodeType(Enum):
 
 
 class DependencyGraphMapper:
+    def __init__(self, dependency_resolver: DependencyResolver) -> None:
+        self._dependency_resolver = dependency_resolver
+
     def map_dependency_graph(self, component_definitions: list[ComponentDefinition]) -> DiGraph:
         self._assert_unique_qualifiers(component_definitions)
 
         graph: DiGraph = DiGraph()
         self._map_components(graph, component_definitions)
-        self._map_dependencies(graph)
+        self._map_dependencies(graph, component_definitions)
 
         return graph
 
@@ -49,59 +50,19 @@ class DependencyGraphMapper:
 
                 graph.add_edge(parameter_node_id, component_node_id)
 
-    def _map_dependencies(self, graph: DiGraph):
-        all_parameters: list[tuple[str, dict[str, Any]]] = self._filter_node_type(graph, NodeType.PARAMETER)
+    def _map_dependencies(self, graph: DiGraph, definitions: list[ComponentDefinition]) -> dict[str, Result[str]]:
+        mapping_results: dict[str, Result[str]] = {}
+        all_parameters: dict[str, Parameter] = self._filter_parameter_nodes(graph)
 
-        for parameter_id, additional_data in all_parameters:
-            properties: Parameter = additional_data['properties']
+        for parameter_id, properties in all_parameters.items():
+            dependency_resolution: Result[str] = self._dependency_resolver.try_resolve(properties, definitions)
 
-            satisfying_component_id: str | None = self._try_find_dependency(
-                graph,
-                properties.kind,
-                properties.qualifier
-            )
+            if dependency_resolution.successful:
+                graph.add_edge(parameter_id, dependency_resolution.value)
 
-            if satisfying_component_id is not None:
-                graph.add_edge(parameter_id, satisfying_component_id)
+            mapping_results[parameter_id] = dependency_resolution
 
-    def _try_find_dependency(self, graph: DiGraph, kind: type, qualifier: str | None) -> str | None:
-        if qualifier is None:
-            return self._find_unqualified_component(graph, kind)
-
-        return self._find_qualified_component(graph, kind, qualifier)
-
-    def _find_qualified_component(self, graph: DiGraph, kind: type, qualifier: str) -> str | None:
-        all_components: list[tuple[str, dict[str, Any]]] = self._filter_node_type(graph, NodeType.COMPONENT)
-
-        for component_id, additional_data in all_components:
-            metadata: ComponentMetadata = additional_data['definition'].component_metadata
-
-            if is_assignable_to(metadata.kind, kind) and metadata.qualifier == qualifier:
-                return component_id
-
-        return None
-
-    def _find_unqualified_component(self, graph: DiGraph, kind: type) -> str | None:
-        all_components: list[tuple[str, dict[str, Any]]] = self._filter_node_type(graph, NodeType.COMPONENT)
-
-        matches: list[tuple[str, ComponentDefinition]] = [
-            (component_id, additional_data['definition'])
-            for component_id, additional_data in all_components
-            if is_assignable_to(additional_data['definition'].component_metadata.kind, kind)
-        ]
-
-        if len(matches) == 1:
-            return matches[0][0]
-
-        primary_matches: list[str] = [
-            component_id for component_id, definition in matches
-            if definition.component_metadata.primary
-        ]
-
-        if len(primary_matches) == 1:
-            return primary_matches[0]
-
-        return None
+        return mapping_results
 
     def _assert_unique_qualifiers(self, component_definitions: list[ComponentDefinition]) -> None:
         for definition in component_definitions:
@@ -121,11 +82,8 @@ class DependencyGraphMapper:
                     f'Qualifiers must be unique.'
                 )
 
-    def _filter_node_type(self, graph: DiGraph, node_type: NodeType) -> list[tuple[str, dict[str, Any]]]:
-        return [
-            (node_id, additional_data) for node_id, additional_data in graph.nodes(data=True)
-            if additional_data['type'] == node_type
-        ]
-
-    def _simplify_parameters(self, component_definition: ComponentDefinition) -> dict[str, type[Any]]:
-        return simplify_arguments(inspect.signature(component_definition.inflator))
+    def _filter_parameter_nodes(self, graph: DiGraph) -> dict[str, Parameter]:
+        return {
+            node_id: additional_data['properties'] for node_id, additional_data in graph.nodes(data=True)
+            if additional_data['type'] == NodeType.PARAMETER
+        }
