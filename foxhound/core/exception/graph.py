e9634834d4746b7f7141ef60326a3ccace8eea5b
Tomash25 <164175207+Tomash25@users.noreply.github.com>
2026-04-03 02:01:42 +0300

    Fix CyclicGraphError fields

--- foxhound/core/exception/graph.py
+++ foxhound/core/exception/graph.py
@@ -1,9 +1,15 @@
+from typing import Any, Hashable
+
 from networkx import DiGraph
 from networkx.algorithms.cycles import simple_cycles
 
 
 class CyclicGraphError(Exception):
+    graph: DiGraph
+    cycles: list[Hashable | Any]
+
     def __init__(self, graph: DiGraph):
-        self.cycles = graph
         formatted_cycles: list[str] = [' → '.join(c) for c in simple_cycles(graph)]
+        self.graph = graph
+        self.cycles = list(simple_cycles(graph))
         super().__init__(f'Cyclic dependencies detected: {"; ".join(formatted_cycles)}')
