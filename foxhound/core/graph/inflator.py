6419c07e7c140e55789bee73e04c2d23a3dfd33b
Tomash25 <164175207+Tomash25@users.noreply.github.com>
2026-04-03 17:19:57 +0300

    Implement DependencyGraphInflator to inflate graph components

---
+++ foxhound/core/graph/inflator.py
@@ -0,0 +1,59 @@
+from typing import Any, Hashable
+
+from networkx.classes import DiGraph
+
+from foxhound.core.container import Container
+from foxhound.core.graph.node_type import NodeType
+from foxhound.core.model.component import Component
+from foxhound.core.model.component_definition import ComponentDefinition
+
+
+class DependencyGraphInflator:
+    def inflate(self, graph: DiGraph, container: Container) -> None:
+        root_components: list[Hashable] = [
+            node for node in graph.nodes() if graph.in_degree(node) == 0
+        ]
+
+        for component_node in root_components:
+            self._inflate_component(component_node, graph, container)
+
+    def _inflate_component(self, component_node: Hashable, graph: DiGraph, container: Container) -> Any:
+        assert graph.nodes[component_node].get('type') == NodeType.COMPONENT
+
+        definition: ComponentDefinition = graph.nodes[component_node].get('definition')
+
+        existing_component: Component[Any] | None = container.get_component(definition.metadata.id)
+
+        if existing_component is not None:
+            return existing_component.value
+
+        inflated_parameters: dict[str, Any] = {
+            parameter: self._inflate_component(dependency_nodes[0], graph, container)
+            for parameter, dependency_nodes in self._parameter_dependency_nodes(component_node, graph).items()
+        }
+
+        inflated_value: Any = definition.inflator(**inflated_parameters)
+
+        container.register_component(
+            Component(
+                metadata=definition.metadata,
+                value=inflated_value,
+            )
+        )
+
+        return inflated_value
+
+    def _parameter_dependency_nodes(self, component_node: Hashable, graph: DiGraph) -> dict[str, list[Hashable]]:
+        assert graph.nodes[component_node].get('type') == NodeType.COMPONENT
+
+        parameter_nodes: list[Hashable] = list(graph.successors(component_node))
+
+        assert all([
+            graph.nodes[parameter_node].get('type') == NodeType.PARAMETER
+            for parameter_node in parameter_nodes
+        ])
+
+        return {
+            graph.nodes[parameter_node].get('properties').name: list(graph.successors(parameter_node))
+            for parameter_node in parameter_nodes
+        }
