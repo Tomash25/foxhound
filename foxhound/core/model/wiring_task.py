cc5f3f9f694d262abfb8d74aebcc9fbb9d4edf57
Tomash25 <164175207+Tomash25@users.noreply.github.com>
2026-04-03 17:41:01 +0300

    Delete WiringTask class

--- foxhound/core/model/wiring_task.py
+++
@@ -1,9 +0,0 @@
-from collections.abc import Callable
-from typing import Any
-
-from foxhound.core.model.base_model import BaseModel
-
-
-class WiringTask(BaseModel):
-    target: Callable[..., Any]
-    completed: bool = False
