65a6b2186083add118ed3d30fa9644e8f4db1b64
Tomash25 <164175207+Tomash25@users.noreply.github.com>
2026-04-03 14:54:36 +0300

    Move CyclicGraphError to graph module

--- foxhound/core/graph/mapper.py
+++ foxhound/core/graph/mapper.py
@@ -2,7 +2,7 @@ from networkx import DiGraph, is_directed_acyclic_graph
 
 from foxhound.core.dependency_resolver import DependencyResolver
 from foxhound.core.exception.dependency import UnsatisfiedDependenciesError
-from foxhound.core.exception.graph import CyclicGraphError
+from foxhound.core.graph.exceptions import CyclicGraphError
 from foxhound.core.graph.node_type import NodeType
 from foxhound.core.model.component_definition import ComponentDefinition
 from foxhound.core.model.parameter import Parameter
