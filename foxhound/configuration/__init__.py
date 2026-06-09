5b45487589f15f8eefbb401a3c36bdc54a7bb0af
Tomash25 <164175207+Tomash25@users.noreply.github.com>
2026-06-09 23:40:31 +0300

    Add ConfigurationReader to configuration module __init__.py

--- foxhound/configuration/__init__.py
+++ foxhound/configuration/__init__.py
@@ -1,5 +1,7 @@
 from foxhound.configuration.api import configuration
+from foxhound.configuration.configuration_reader import ConfigurationReader
 
 __all__ = [
-    'configuration'
+    'configuration',
+    'ConfigurationReader'
 ]
