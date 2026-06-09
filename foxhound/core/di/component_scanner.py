a260dbedc89b5f1dff77c24ebf22e84356916236
Tomash25 <164175207+Tomash25@users.noreply.github.com>
2026-06-09 23:38:01 +0300

    Change ComponentScanner to use sys.modules

--- foxhound/core/di/component_scanner.py
+++ foxhound/core/di/component_scanner.py
@@ -1,31 +1,39 @@
-import importlib
-import inspect
-import pkgutil
-from collections.abc import Iterator
+import sys
 from types import ModuleType
+from typing import Any
 
 from foxhound.core.di.consts import OBJECT_COMPONENT_DEFINITION_ATTRIBUTE
 from foxhound.core.di.models import ComponentDefinition
 
 
 class ComponentScanner:
-    def __init__(self, module: str):
-        self._module = module
-
     def scan(self) -> list[ComponentDefinition]:
+        seen: set[str] = set()
         definitions: list[ComponentDefinition] = []
-        package: ModuleType = importlib.import_module(self._module)
-        module_iterator: Iterator[pkgutil.ModuleInfo] = pkgutil.walk_packages(
-            path=package.__path__,
-            prefix=package.__name__ + '.',
-            onerror=lambda _: None
-        )
 
-        for _, module_name, _ in module_iterator:
-            module: ModuleType = importlib.import_module(module_name)
-            for _, obj in inspect.getmembers(module):
-                definition: ComponentDefinition | None = getattr(obj, OBJECT_COMPONENT_DEFINITION_ATTRIBUTE, None)
-                if definition is not None:
-                    definitions.append(definition)
+        for module in list(sys.modules.values()):
+            if module is None:
+                continue
+
+            module_definitions: list[ComponentDefinition] = self._collect_from_module(module)
+
+            for definition in module_definitions:
+                if definition.metadata.id in seen:
+                    continue
+
+                seen.add(definition.metadata.id)
+                definitions.append(definition)
 
         return definitions
+
+    def _collect_from_module(self, module: ModuleType) -> list[ComponentDefinition]:
+        collected_definitions: list[ComponentDefinition] = []
+        module_members: list[Any] = list(vars(module).values())
+
+        for obj in module_members:
+            definition: ComponentDefinition | None = getattr(obj, OBJECT_COMPONENT_DEFINITION_ATTRIBUTE, None)
+
+            if type(definition) is ComponentDefinition:
+                collected_definitions.append(definition)
+
+        return collected_definitions
