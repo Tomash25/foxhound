dc40e24dd8e5753c228ac7d50b05007997a60cc7
Tomash25 <164175207+Tomash25@users.noreply.github.com>
2026-08-01 16:54:18 +0300

    Bump package version to 0.1.4

--- foxhound/__init__.py
+++ foxhound/__init__.py
@@ -7,7 +7,7 @@ from foxhound.core.di.container import Container
 from foxhound.core.di.models import Component, ComponentDefinition, ComponentMetadata
 from foxhound.core.models import Result
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 __all__ = [
     'component',
